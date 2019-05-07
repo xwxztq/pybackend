b8396a68614eef73b220e1b95f1222ea01ebbf44
San <834026555@qq.com>
2019-05-07 20:07:59 +0800

    nothing new

--- climax4musics/views.py
+++ climax4musics/views.py
@@ -1,8 +1,22 @@
+import os
+import sys
+
+print(sys.path)
+
+print(sys.path)
 from django.shortcuts import render
 from django.http import HttpResponse
 from climax4musics.climax import get_climax
 
 def climax(request):
+
+    cur_pwd = os.getcwd()
+    if cur_pwd in sys.path:
+        pass
+    else:
+        sys.path.append(cur_pwd+"climax")
+
+    print(sys.path)
 
     if request.method == 'POST':
 
