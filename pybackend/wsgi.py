9ccd504939d9a8d43e2297a2602799c71e0ed10a
San <834026555@qq.com>
2019-05-12 15:13:40 +0800

    fix the modules problems

--- pybackend/wsgi.py
+++ pybackend/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pybackend.settings')
+
+cur_pwd = os.getcwd()
+
+w2m = os.path.join(cur_pwd,"wav2mids","w2m")
+climax = os.path.join(cur_pwd,"climax4musics","climax")
+
+sys.path.append(w2m)
+sys.path.append(climax)
+
+print(sys.path)
+
 
 application = get_wsgi_application()
