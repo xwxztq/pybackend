ad64c14c5984d989fb4e8f05f3e4ece329ee9efd
San <834026555@qq.com>
2019-05-14 23:59:27 +0800

    update the midi2xml views

--- midi2xmls/views.py
+++ midi2xmls/views.py
@@ -14,30 +14,24 @@ def transfer(request):
         purename = f1.name.split('.')
         # fname = 'static/media/car/a.png'
         print(fname)
+        with open(fname, 'wb+') as pic:
+            # 根据上传的流中的数据一点一点往内存中写
+            for c in f1.chunks():
+                pic.write(c)
+        score = music21.converter.parse(fname)
+        ext = music21.stream.Stream(score)
+        mid_path = os.path.join(MEDA_PATH, "mid", purename[0], ".mid")
         try:
-            with open(fname, 'wb+') as pic:
-                # 根据上传的流中的数据一点一点往内存中写
-                for c in f1.chunks():
-                    pic.write(c)
+            ext.write('xml',fp=mid_path)
         except:
             response = HttpResponse()
             response.status_code = 500
-            response.content = "上传文件写入时出错"
+            response.content = "xml文件写入时出错"
         else:
-            score = music21.converter.parse(fname)
-            ext = music21.stream.Stream(score)
-            mid_path = os.path.join(MEDA_PATH, "mid", purename[0], ".mid")
-            try:
-                ext.write('xml',fp=mid_path)
-            except:
-                response = HttpResponse()
-                response.status_code = 500
-                response.content = "xml文件写入时出错"
-            else:
-                with open(mid_path,"rb+") as ret:
-                    response = FileResponse(ret)
-                    response['Content-Type'] = 'application/msword'
-                    response['Content-Disposition'] = 'attachment;filename=' + purename[0]+".mid"
+            with open(mid_path,"rb+") as ret:
+                response = FileResponse(ret)
+                response['Content-Type'] = 'application/msword'
+                response['Content-Disposition'] = 'attachment;filename=' + purename[0]+".mid"
     else:
         response = HttpResponse("HTTP请求错误")
         response.status_code = 400
