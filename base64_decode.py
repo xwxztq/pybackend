d8e855f1b20e27973a2f03db4b198c4fa05cf2ca
San <834026555@qq.com>
2019-05-16 03:10:40 +0800

    add a module

---
+++ base64_decode.py
@@ -0,0 +1,32 @@
+import re
+import base64
+
+
+def ToBase64(file, txt):
+    with open(file, 'rb') as fileObj:
+        image_data = fileObj.read()
+        base64_data = base64.b64encode(image_data)
+        fout = open(txt, 'w')
+        fout.write(base64_data.decode())
+        fout.close()
+
+
+def decode_base64(data, altchars=b'+/'):
+    """Decode base64, padding being optional.
+
+    :param data: Base64 data as an ASCII byte string
+    :returns: The decoded byte string.
+
+    """
+    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
+    missing_padding = len(data) % 4
+    if missing_padding:
+        data += b'=' * (4 - missing_padding)
+    return base64.b64decode(data, altchars)
+
+
+def transfer(data):
+    the_head, pure_data = data.splot(',')
+    the_format = the_head.split(';')[0].splot('/')[1]
+
+    return decode_base64(pure_data.encode()), the_format
--- midi2xmls/views.py
+++ midi2xmls/views.py
@@ -4,32 +4,9 @@ from django.http import FileResponse, HttpResponse
 from pybackend.settings import MEDA_PATH
 import music21
 from binascii import a2b_base64
-import base64
 import datetime
-import re
+import base64_decode
 
-
-def ToBase64(file, txt):
-    with open(file, 'rb') as fileObj:
-        image_data = fileObj.read()
-        base64_data = base64.b64encode(image_data)
-        fout = open(txt, 'w')
-        fout.write(base64_data.decode())
-        fout.close()
-
-
-def decode_base64(data, altchars=b'+/'):
-    """Decode base64, padding being optional.
-
-    :param data: Base64 data as an ASCII byte string
-    :returns: The decoded byte string.
-
-    """
-    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
-    missing_padding = len(data) % 4
-    if missing_padding:
-        data += b'=' * (4 - missing_padding)
-    return base64.b64decode(data, altchars)
 
 
 def ToFile(txt, file):
@@ -48,30 +25,34 @@ def transfer(request):
     if request.method == "POST":
         content = request.POST.get('file')
 
-        content = content.split(',')[1]
+        the_format,the_content = base64_decode.transfer(content)
 
-        ori_image_data = decode_base64(content.encode())
+        if the_format != 'mid':
+            response = HttpResponse()
+            response.status_code = 400
+            response.content = "格式不正确"
 
-        pure_name = str(datetime.datetime.now())
+        else:
+            pure_name = str(datetime.datetime.now())
 
-        fname = os.path.join(MEDA_PATH, "mid", pure_name+".mid")
+            fname = os.path.join(MEDA_PATH, "mid", pure_name+'.'+the_format)
 
-        with open(fname, 'wb') as fout:
-            fout.write(ori_image_data)
+            with open(fname, 'wb') as fout:
+                fout.write(ori_image_data)
 
 
-        score = music21.converter.parse(fname)
-        ext = music21.stream.Stream(score)
-        xml_path = os.path.join(MEDA_PATH, "xml", pure_name+".xml")
-        ret_path = "http://47.99.83.172/files/download?name=xml/" + pure_name+".xml"
-        ext.write('xml', fp=xml_path)
-        response = HttpResponse()
-        response.content = ret_path
-        response.status_code = 200
-        # ret = open(xml_path,"rb")
-        # response = FileResponse(ret)
-        # response['Content-Type'] = 'application/msword'
-        # response['Content-Disposition'] = 'attachment;filename=' + purename[0]+".xml"
+            score = music21.converter.parse(fname)
+            ext = music21.stream.Stream(score)
+            xml_path = os.path.join(MEDA_PATH, "xml", pure_name+".xml")
+            ret_path = "http://47.99.83.172/files/download?name=xml/" + pure_name+".xml"
+            ext.write('xml', fp=xml_path)
+            response = HttpResponse()
+            response.content = ret_path
+            response.status_code = 200
+            # ret = open(xml_path,"rb")
+            # response = FileResponse(ret)
+            # response['Content-Type'] = 'application/msword'
+            # response['Content-Disposition'] = 'attachment;filename=' + purename[0]+".xml"
     elif request.method == "OPTIONS":
         response = HttpResponse()
         response['Access-Control-Allow-Method'] = "POST"
