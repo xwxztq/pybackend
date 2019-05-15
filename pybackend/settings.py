6ab45a37950d371e51a3932b6867b35fe539d4ad
San <834026555@qq.com>
2019-05-15 09:47:32 +0800

    fix the static route

--- pybackend/settings.py
+++ pybackend/settings.py
@@ -15,7 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDA_PATH = os.path.join(os.getcwd(),"static")
+# MEDA_PATH = os.path.join(os.getcwd(),"static")
+
+MEDA_PATH = "static/"
 
 
 # Quick-start development settings - unsuitable for production
